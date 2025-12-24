52cb59c5406c44febdedc98f811b1b79282e85cf
이예지 <iyeji@MacBookAir.lan>
2025-12-24 13:07:55 +0100

    Changement script

--- programmes/urls-to-table.sh
+++ programmes/urls-to-table.sh
@@ -1,26 +1,27 @@
+output_dir="tableaux"
+output_file="$output_dir/.html"
+fichier_urls="urls.txt"
+
+mkdir -p "$output_dir"
+
 {
-echo "<html>
-<head><meta charset=\"UTF-8\"></head>
-<body>
-<table>
-<tr><th>numero</th><th>URL</th><th>code</th><th>encodage</th></tr>"
+    echo "<html>"
+    echo "<head><meta charset=\"UTF-8\"></head>"
+    echo "<body>"
+    echo "<table border='1'>"
+    echo "<tr><th>numero</th><th>URL</th><th>code</th><th>encodage</th></tr>"
+    
+    lineno=1
+    while read -r line; do
+        read http_code content_type <<< $(curl -s -L -o /dev/null -w "%{http_code} %{content_type}" "$line")
+        encoding=$(echo "$content_type" | grep -o "charset=\S+" | cut -d= -f2)
+        encoding=${encoding:-"N/A"}
+    
+        echo "<tr><td>$lineno</td><td>$line</td><td>$http_code</td><td>$encoding</td></tr>"
+        lineno=$((lineno+1))
+    done < "$fichier_urls"
 
-lineno=1
-while read -r line
-do
-    read http_code content_type <<< $(curl -s -L -o /dev/null -w "%{http_code} %{content_type}" "$line")
-    encoding=$(echo "$content_type" | grep -o "charset=\S+" | cut -d= -f2)
-    encoding=${encoding:-"N/A"}
-
-    echo "<tr>
-<td>$lineno</td>
-<td>$line</td>
-<td>$http_code</td>
-<td>$encoding</td>
-</tr>"
-
-    lineno=$((lineno+1))
-done < "$fichier_urls"
-
-echo "</table></body></html>"
-} > tableaux/kr.html
+    echo "</table>"
+    echo "</body>"
+    echo "</html>"
+} > "$output_file"
